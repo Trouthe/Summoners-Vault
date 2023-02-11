f829bad62e448c54a8c28a9559897e96c699cd05
Trouthe <anthony.hajjar00@gmail.com>
2023-02-11 04:06:14 +0200

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { AccInfoComponent } from './acc-info/acc-info.component';
+
+const routes: Routes = [
+  {path: "", pathMatch: "full", redirectTo: "home"},
+  {path: "home", component: HomeComponent},
+  {path: "accountinfo", component: AccInfoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AccInfoComponent } from './acc-info/acc-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AccInfoComponent
   ],
   imports: [
     BrowserModule,

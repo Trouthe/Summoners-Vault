982718e441cbad8c44fd37eba5b3e04d34802e47
Trouthe <anthony.hajjar00@gmail.com>
2023-05-22 04:07:44 +0300

    add account (phase 2)

--- src/app/add-account/add-account.component.ts
+++ src/app/add-account/add-account.component.ts
@@ -1,27 +1,105 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-add-account',
   templateUrl: './add-account.component.html',
-  styleUrls: ['./add-account.component.css']
+  styleUrls: ['./add-account.component.css'],
 })
 export class AddAccountComponent {
-  
   uid: string | null = this.authService.getUID();
-  accVerification: string = 'no';
-  serverOptions: any = ['Europe West','Europe Nodric East', 'North America'];
-  
-  constructor (
+
+  accountForm!: FormGroup;
+  showEmailFields: boolean = false;
+
+  accountName!: FormControl;
+  accountPassword!: FormControl;
+  summonerDisplayName!: FormControl;
+
+  constructor(
     private authService: AuthService,
     private router: Router,
-  ) { }
+    private formBuilder: FormBuilder,
+    private http: HttpClient
+  ) {}
 
   ngOnInit(): void {
     // is logged in?
-    if (!this.authService.isloggedIn()) 
-      this.router.navigate(['/login']);
+    if (!this.authService.isloggedIn()) this.router.navigate(['/login']);
+
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.accountName = new FormControl('', Validators.required);
+    this.accountPassword = new FormControl('', Validators.required);
+    this.summonerDisplayName = new FormControl('', Validators.required);
+
+    this.accountForm = this.formBuilder.group({
+      userID: this.uid,
+      accountName: this.accountName,
+      accountPassword: this.accountPassword,
+      summonerDisplayName: this.summonerDisplayName,
+      emailVerification: 0,
+      accountEmail: '',
+      accountEmailPassword: '',
+      server: 'EUW1',
+      blueEssence: 0,
+      orangeEssence: 0,
+      riotPoints: 0,
+      skins: 0,
+      accountLevel: 0,
+      championsOwned: 0,
+    });
+  }
+
+  onSubmit() {
+    if (this.accountForm.valid) {
+      // Access the form values
+      const formValues = this.accountForm.value;
+      console.log(formValues);
+
+      const formData = new FormData();
+      for (const key in formValues) {
+        formData.append(key, formValues[key]);
+      }
+
+      this.http.post('https://spicysalmon.000webhostapp.com/insertAccount.php', formData, {responseType: 'text'}).subscribe(
+        () => {
+          console.log('Data inserted successfully!');
+        },
+        (error) => {
+          console.error('Failed to insert data:', error);
+        }
+      );
+      // Reset the form and keep the default selected values
+      this.accountForm.reset({
+        userID: this.uid,
+        emailVerification: formValues.emailVerification,
+        accountEmail: '',
+        accountEmailPassword: '',
+        server: 'EUW1',
+        blueEssence: 0,
+        orangeEssence: 0,
+        riotPoints: 0,
+        skins: 0,
+        accountLevel: 0,
+        championsOwned: 0,
+      });
+    } else {
+      // Mark all form controls as touched to display validation errors
+      this.markAllFormControlsAsTouched();
+    }
+  }
+
+  markAllFormControlsAsTouched() {
+    Object.values(this.accountForm.controls).forEach((control) => {
+      control.markAsTouched();
+    });
   }
 
   logoutFunction() {
